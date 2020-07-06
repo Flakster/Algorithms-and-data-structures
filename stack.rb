134b18fc68e53e524b57c7dfeb67b8ffd4c1566d
flakster <carlos.santamaria@gmail.com>
2020-07-06 12:33:30 -0500

    Add code for the min stack repl.it challenge

--- stack.rb
+++ stack.rb
@@ -18,27 +18,48 @@ class Stack
     remove(0)
     value
   end
+
+  def min
+    current = self.head
+    current && min = current.value
+    while !current.nil?
+      current.value && current.value < min && min = current.value
+      current = current.next_node
+    end
+    min
+  end
 end
 
 stack = Stack.new
+
+
 stack.push(3)
 
 stack.push(5)
-puts stack.pop
 
-# => 5
+puts stack.min
+
+# => 3
+
+
+stack.pop
+
+stack.push(7)
+
+puts stack.min
+
+# => 3
 
 
 stack.push(2)
-stack.push(7)
-puts stack.pop
-# => 7
 
-puts stack.pop
+puts stack.min
 
 # => 2
 
 
-puts stack.pop
+stack.pop
+
+puts stack.min
 
 # => 3
