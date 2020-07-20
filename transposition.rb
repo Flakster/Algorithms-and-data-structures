32e59207449ab506401556f4c040b983264bc0c7
flakster <carlos.santamaria@gmail.com>
2020-07-20 11:21:00 -0500

    Add Transposition codding challenge

---
+++ transposition.rb
@@ -0,0 +1,33 @@
+def transpose(string)
+  output = ''
+  n = 0
+  g = 0
+  string.split('').each_with_index do |c,i|
+    if ['g','n'].include?(c)
+      g += 1 if c == 'g' 
+      n += 1 if c == 'n'
+    else
+      n.times { output << 'n' }
+      g.times { output << 'g' }
+      output << c
+      n = 0
+      g = 0
+    end
+  end
+  n.times { output << 'n' }
+  g.times { output << 'g' }
+  return output
+end
+
+#transpose('he was searchign on Bign for signign kigns')
+#transpose('rignadingdiggn!')
+#transpose('gngngnnggnngggnnn')
+
+puts transpose('he was searchign on Bign for signign kigns')
+# => he was searching on Bing for singing kings
+
+puts transpose('rignadingdiggn!')
+# => ringadingdingg!
+
+puts transpose('gngngnnggnngggnnn')
+# => nnnnnnnnngggggggg
