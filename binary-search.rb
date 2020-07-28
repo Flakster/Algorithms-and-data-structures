72f941f729b0f5888eecf817cf07d06e9effd6ea
flakster <carlos.santamaria@gmail.com>
2020-07-27 19:48:25 -0500

    Add the binary search recursive challenge

---
+++ binary-search.rb
@@ -0,0 +1,15 @@
+def sqrt(number)
+  sqrt_recursive(number, 0, number)
+end
+
+def sqrt_recursive(number, min_interval, max_interval)
+  # Your code here
+  guess = (min_interval + max_interval)  / 2
+  return guess if guess * guess == number
+  (guess * guess) < number ? sqrt_recursive(number, guess, max_interval) : sqrt_recursive(number, min_interval,guess)
+end
+
+puts sqrt(25)
+puts sqrt(7056)
+puts sqrt(144)
+puts sqrt(625)
