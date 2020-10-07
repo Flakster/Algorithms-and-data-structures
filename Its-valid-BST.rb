364ca8c3772410f33bcb75292a1ad6efcd6f3289
flakster <carlos.santamaria@gmail.com>
2020-10-07 11:59:42 -0500

    Add the it is BST challenge'

---
+++ Its-valid-BST.rb
@@ -0,0 +1,43 @@
+class Node
+	attr_reader :data
+	attr_accessor :left, :right
+	
+	def initialize data
+		@data = data
+	end
+end
+
+def array_to_tree(array, index = 0)
+  # use your function from the previous challenge
+  return nil if array.length == 0 || index >= array.length || array[index] == 0
+
+  node = Node.new(array[index])
+  node.left = array_to_tree(array, 2 * index + 1)
+  node.right = array_to_tree(array, 2 * index + 2)
+
+  return node
+end
+
+def search_tree?(array)
+	root = array_to_tree(array)
+  # write your code here
+  return traverse(root)
+end
+
+def traverse(node, min = nil, max = nil)
+  return true if node.nil?
+    
+  return false if min && node.data < min
+
+  return false if max && node.data > max
+
+  traverse(node.left, min, node.data) && traverse(node.right, node.data, max)
+end
+
+puts search_tree?([10, 4, 12])
+# => true
+
+puts search_tree?([10, 5, 7])
+# => false
+
+puts search_tree?([20, 10, 27, 12, 14, 23, 30])
