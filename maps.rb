29f157a0810438e3e74cdf9927fa9b01a4f4304d
flakster <carlos.santamaria@gmail.com>
2020-10-19 13:14:46 -0500

    Add maps - hashes challenge

---
+++ maps.rb
@@ -0,0 +1,22 @@
+def appears_most_times(array)
+  # write your code here
+  myhash = Hash.new
+  array.each do |e|
+    if myhash[e].nil?
+      myhash[e] = 1
+    else
+      myhash[e] += 1
+    end
+  end
+  result = myhash.max_by{|k,v| v}
+  return result[0]
+end
+
+puts appears_most_times([1, 2, 3, 1, 5])
+# => 1
+
+puts appears_most_times([23, 43, 67, 88, 42, 35, 77, 88, 99, 11])
+# => 88
+
+puts appears_most_times([4376, -345, -345, 4376, -345, 84945, 4376, -345, -26509, 4376, 84945, 84945, -26509, 896341, 4376])
+# => 4376
