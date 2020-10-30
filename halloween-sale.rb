8eb8207af9d7a23c26633992cf11b417d18155bb
flakster <carlos.santamaria@gmail.com>
2020-10-30 12:50:43 -0500

    Add the howManyGames method

--- halloween-sale.rb
+++ halloween-sale.rb
@@ -1,2 +1,13 @@
 
-
+def howManyGames(p, d, m, s)
+  # Return the number of games you can buy
+  remaining = s
+  current = p
+  amount = 0
+  while remaining > current
+    amount += 1
+    remaining -= current
+    current = current - d >= m ? current - d : m
+  end
+  amount
+end
