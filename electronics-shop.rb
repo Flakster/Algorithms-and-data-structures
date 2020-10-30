6da83f65767ee446d2fb8ff28912d28d44adc62e
flakster <carlos.santamaria@gmail.com>
2020-10-30 11:22:47 -0500

    Add the getMoneySpent method

---
+++ electronics-shop.rb
@@ -0,0 +1,14 @@
+def getMoneySpent(keyboards, drives, b)
+  # Write your code here.
+  max = -1
+  keyboards.each do |k|
+    if k < b
+      drives.each do |d|
+        if k + d < b && k + d > max
+          max = k + d
+        end
+      end
+    end
+  end
+  max
+end
