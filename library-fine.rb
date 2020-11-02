120d369162733a2b07295ee1c09f0834d5f58855
flakster <carlos.santamaria@gmail.com>
2020-11-02 15:55:07 -0500

    Add the code to handle a day fine or no fine

--- library-fine.rb
+++ library-fine.rb
@@ -1,4 +1,6 @@
 def libraryFine(d1, m1, y1, d2, m2, y2)
   return 10000 if (y2-y1).positive?
-  return (m2 - m1) * 500 if (m2-m1).positive?
+  return (m2 - m1) * 500 if (m2 - m1).positive?
+  return (d2 - d1) * 15 if (d2 - d1).positive?
+  0
 end
