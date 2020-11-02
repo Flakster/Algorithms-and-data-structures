c7e728df2e0bc23e42c38f36ef2d2d1ba14c6524
flakster <carlos.santamaria@gmail.com>
2020-11-02 16:11:50 -0500

    Fix the code to handle uncover edge cases

--- library-fine.rb
+++ library-fine.rb
@@ -1,8 +1,10 @@
 def libraryFine(d1, m1, y1, d2, m2, y2)
-  return 10000 if (y1-y2).positive?
-  return (m1 - m2) * 500 if (m1 - m2).positive?
+  return 10000 if y1 - y2 > 0
+  return 0 if y1 - y2 < 0 
+  return (m1 - m2) * 500 if m1 - m2 > 0
+  return 0 if m1 - m2 < 0
   return (d1 - d2) * 15 if (d1 - d2).positive?
-  0
+  0|
 end
 
 p libraryFine(9,6,2015,6,6,2015)
