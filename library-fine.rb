2e2eea3bab364f94518bcde3bd14aadc6f2b67ae
flakster <carlos.santamaria@gmail.com>
2020-11-02 15:57:34 -0500

    Fix a bug in the order of the dates

--- library-fine.rb
+++ library-fine.rb
@@ -1,6 +1,6 @@
 def libraryFine(d1, m1, y1, d2, m2, y2)
-  return 10000 if (y2-y1).positive?
-  return (m2 - m1) * 500 if (m2 - m1).positive?
-  return (d2 - d1) * 15 if (d2 - d1).positive?
+  return 10000 if (y1-y2).positive?
+  return (m1 - m2) * 500 if (m1 - m2).positive?
+  return (d1 - d2) * 15 if (d1 - d2).positive?
   0
 end
