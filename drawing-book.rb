1f3bd8271c1b6657a0bed7777e3388081b9a7ca5
flakster <carlos.santamaria@gmail.com>
2020-11-03 18:34:32 -0500

    Fix a bug calculating tl (turns to the left)

--- drawing-book.rb
+++ drawing-book.rb
@@ -3,7 +3,6 @@ def pageCount(n, p)
   # Write your code here.
   #
   tr = p / 2
-  tl = n.even? ? (n + 1 - p) / 2 : (n + 1) / 2
+  tl = n.even? ? (n + 1 - p) / 2 : (n - p) / 2
   [tr,tl].min
 end
-
