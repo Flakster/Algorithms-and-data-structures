72a36529c853bc708272fba634804c4e2918ecdf
flakster <carlos.santamaria@gmail.com>
2020-11-04 17:24:28 -0500

    Add the code to solve the challenge

--- picking-numbers.rb
+++ picking-numbers.rb
@@ -1,4 +1,19 @@
 def pickingNumbers(a)
   # Write your code here
-
+  myHash = {}
+  a.each do |e|
+    myHash[e].nil? ? myHash[e] = 1 : myHash[e] += 1
+  end
+  last = -1
+  return myHash[myHash.keys[0]] if myHash.length == 1
+  max = 0
+  myHash.keys.sort.each do |e|
+    if last > 0
+      if e - last <= 1 && myHash[e] + myHash[last] > max
+        max = myHash[e] + myHash[last]
+      end
+    end
+    last = e
+  end
+  max
 end
