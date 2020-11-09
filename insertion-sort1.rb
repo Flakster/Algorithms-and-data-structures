e0c8465a7c97a024c38fa38ae5a795fa53521984
flakster <carlos.santamaria@gmail.com>
2020-11-09 11:33:40 -0500

    Fix a logic error

--- insertion-sort1.rb
+++ insertion-sort1.rb
@@ -5,13 +5,17 @@ def insertion_sort(array)
   while index >= 0 do
     if array[index] > element
       array[index + 1] = array[index]
+      if index.zero?
+        puts array.join(' ')
+        array[index] = element
+      end
       index -= 1
     else
       array[index + 1] = element
       index = -1
     end
-    p array.join(' ')
+    puts array.join(' ')
   end
 end
 
-insertion_sort([1, 4, 6, 9, 3])
+insertion_sort([2, 3, 4, 5, 6, 7, 8, 9, 10, 1])
