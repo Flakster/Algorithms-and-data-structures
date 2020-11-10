bda535cecf476140d50099a5a508a4a449661bd4
flakster <carlos.santamaria@gmail.com>
2020-11-10 18:48:52 -0500

    Refactor the method to make it shorter and cleaner

--- equalize-the-array.rb
+++ equalize-the-array.rb
@@ -1,11 +1,7 @@
 def equalizeArray(arr)
   freq_hash = {}
   arr.each do |e|
-    if freq_hash[e].nil?
-      freq_hash[e] = 1
-    else
-      freq_hash[e] += 1
-    end
+    freq_hash[e].nil? ? freq_hash[e] = 1 : freq_hash[e] += 1
   end
   arr.length - freq_hash.values.max
 end
