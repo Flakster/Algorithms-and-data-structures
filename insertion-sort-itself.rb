6d7a4560837334dd0d0cdd87efa762ab5f197b09
flakster <carlos.santamaria@gmail.com>
2020-11-10 18:37:06 -0500

    Create the file to solve the challenge

--- insertion-sort-itself.rb
+++ insertion-sort-itself.rb
@@ -1,4 +1,14 @@
 def sort_itself(array)
   # write your code here
-  
+  array.each_with_index do |e,i|
+    next if i < 1
+    (i).downto(0).each do |u|
+      if u > 0 && array[i] < array[u-1]
+        array[u-1],array[i] = array[i],array[u-1]
+        p array.join(' ')
+      end
+    end
+  end
 end
+
+sort_itself([1, 4, 3, 6, 9, 2])
