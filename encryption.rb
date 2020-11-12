e10b7a17b78cbb33a7150f83834216231dd2c14c
flakster <carlos.santamaria@gmail.com>
2020-11-11 20:17:24 -0500

    Add code to build the bidimensional array

--- encryption.rb
+++ encryption.rb
@@ -5,4 +5,16 @@ def encryption(s)
   if rows*cols < strLength
     rows +=1
   end
+  arr = s.split('')
+  arr2 = []
+  while arr.length >= cols do
+    arr2 << arr.shift(cols)
+  end
+  if arr.length > 0
+    arr2 << arr
+  end
+  arr2
 end
+
+
+p encryption('chillout')
