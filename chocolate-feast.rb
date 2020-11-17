abb3d52f1863e87f74af6a78434ee33780611539
flakster <carlos.santamaria@gmail.com>
2020-11-16 19:19:12 -0500

    Add the code to solve the challenge

--- chocolate-feast.rb
+++ chocolate-feast.rb
@@ -1,4 +1,12 @@
 def chocolateFeast(n, c, m)
-
-
+  total = 0
+  num = n/c
+  total += num
+  remainder = 0
+  while num + remainder >= m do
+    num = (num + remainder) / m
+    remainder = (num + remainder) % m 
+    total +=num
+  end
+  total
 end
