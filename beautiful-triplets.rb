a22a374845d6a832d472638bcbb7b40e7046e292
flakster <carlos.santamaria@gmail.com>
2020-11-17 19:49:54 -0500

    Add the code to solve the challlenge

--- beautiful-triplets.rb
+++ beautiful-triplets.rb
@@ -1,4 +1,15 @@
 def beautifulTriplets(d, arr)
-
-
+  total = 0
+  (2..arr.length-1).each do |l|
+    (l-1).downto(1).each do |m|
+      if arr[l] - arr[m] == d
+        (m-1).downto(0) do |f|
+          if arr[m]-arr[f] == d
+            total +=1
+          end
+        end
+      end
+    end
+  end
+  total
 end
