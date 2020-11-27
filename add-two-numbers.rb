5e323f57751ac6c4e35c2dbdd232ff0265513e09
flakster <carlos.santamaria@gmail.com>
2020-11-26 20:32:25 -0500

    First iteration

--- add-two-numbers.rb
+++ add-two-numbers.rb
@@ -10,5 +10,14 @@ end
 # @param {ListNode} l2
 # @return {ListNode}
 def add_two_numbers(l1, l2)
-
+  node1 = l1
+  node2 = l2
+  carry = false
+  first_value = node1.val + node2.val
+  if first_value > 9
+    first_value -= 10
+    carry = true
+  end
+  result = ListNode.new(first_value)
+  
 end
