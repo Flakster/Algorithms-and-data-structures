c7deea4b824bd82ce91d220596684b722e789f3e
flakster <carlos.santamaria@gmail.com>
2020-11-27 18:47:34 -0500

    Add the code to solve the challenge

--- lisas-workbook.rb
+++ lisas-workbook.rb
@@ -1,4 +1,18 @@
 def workbook(n, k, arr)
-
-
+  result = 0 
+  page = 1
+  arr.each do |problems|
+    number = 1 # number of problem in the page
+    problem = 1
+    while problem <= problems
+      result += 1 if problem == page
+      problem +=1
+      number +=1
+      if number > k
+        page += 1
+        number = 1
+      end
+    end
+  end
+  return result
 end
