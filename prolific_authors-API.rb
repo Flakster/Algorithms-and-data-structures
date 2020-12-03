d754113d917e1c323cfb35b1f33eb5b8358ac25e
flakster <carlos.santamaria@gmail.com>
2020-12-03 15:25:09 -0500

    Add the code to solve the challenge

--- prolific_authors-API.rb
+++ prolific_authors-API.rb
@@ -1,2 +1,29 @@
 # Get the info from the API and return a list of authors with a submission count strictly 
 # higher than the threshold
+
+require 'net/http'
+require 'json'
+
+def getUsernames(threshold)
+    # Write your code here
+    arr = []
+    page = 1
+    url = "https://jsonmock.hackerrank.com/api/article_users?page=#{page}"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    results = JSON.parse(response.to_s)
+    data = results['data']
+    while data.length > 0 
+      data.each do |e|
+        p "pag: #{page} id: #{e['id']} username: #{e['username']} sub. count: #{e['submission_count']}"
+        arr << e['username'] if e['submission_count'] > threshold
+      end
+      page += 1
+      url = "https://jsonmock.hackerrank.com/api/article_users?page=#{page}"
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      results = JSON.parse(response.to_s)
+      data = results['data']  
+    end
+    return arr
+end
