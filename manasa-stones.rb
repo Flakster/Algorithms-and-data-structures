4a4064bd4368fabf00eb8e03c73c44eb9a51b51c
flakster <carlos.santamaria@gmail.com>
2020-12-03 17:12:10 -0500

    Fix a bug

--- manasa-stones.rb
+++ manasa-stones.rb
@@ -1,2 +1,17 @@
-def stones(n)
+def stones(n, a, b)
+  arr1 = [0]
+  i = 1
+  while i < n
+    p i
+    arr2 =[]
+    arr1.each do |e|
+      arr2 << e + a
+      arr2 << e + b
+    end
+    arr2.uniq!
+    arr1 = arr2
+    i += 1
+  end
+  arr2.sort
 end
+
