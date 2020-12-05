4a4721406db63fadf11667a8a2764af769a62349
flakster <carlos.santamaria@gmail.com>
2020-12-04 20:30:16 -0500

    Fix a bug

--- encryption.rb
+++ encryption.rb
@@ -2,17 +2,18 @@ def encryption(s)
   arr = s.split('')
   cols = Math.sqrt(arr.length).ceil
   rows = Math.sqrt(arr.length).floor
-  rows +=1 if rows*cols < strLength
+  rows +=1 if rows*cols < arr.length
+  result = []
   arr2 = []
   while arr.length > 0 do
     arr2 << arr.shift(cols)
   end
   (0..cols-1).each do |c|
     word = []
-    (0..rows-1).each do|r|
+    (0..rows-1).each do |r|
       word << (arr2[r][c].nil? ? ' ' : arr2[r][c])
     end
-    word = "#{word} " unless word[-1] == ' '
+    word << ' ' unless word[-1] == ' '
     result << word.join('')
   end
   return result.join('')
