f4c97494b81647686ce4f5666a7bb981b642f031
flakster <carlos.santamaria@gmail.com>
2020-12-07 20:58:15 -0500

    Add the code to solve the challenge

--- happy-leidybug.rb
+++ happy-leidybug.rb
@@ -1,4 +1,14 @@
 def happyLadybugs(b)
-
-
+  freq = {}
+  b.split('').each do |e|
+    freq[e] = freq[e].nil? ? 1 : freq[e] += 1 
+  end
+  freq.keys.each do |k|
+    if k == '_'
+      return 'NO' if freq[k] < 1
+    else
+      return 'NO' if freq[k] < 2
+    end
+  end
+  return 'YES'
 end
