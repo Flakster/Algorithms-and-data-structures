2203fbaa76e3edc242d4da8a5658da9d0f8b1228
flakster <carlos.santamaria@gmail.com>
2021-01-12 18:02:03 -0500

    add the third manual test case from repl.it

--- number_of_primes.rb
+++ number_of_primes.rb
@@ -16,3 +16,6 @@ puts number_of_primes([2, 3, 5, 6, 9])
 
 puts number_of_primes([121, 17, 21, 29, 11, 341, 407, 19, 119, 352])
 # => 4
+
+puts number_of_primes([7, 6, 7, 3, 77, 14, 28, 35, 42])
+# => 3
