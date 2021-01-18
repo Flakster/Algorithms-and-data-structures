0b6f1ed1bcf757061eaf5546a29ae08bf3aa53a1
flakster <carlos.santamaria@gmail.com>
2021-01-18 14:22:50 -0500

    make sum hash cummulative

--- counting_sort1.rb
+++ counting_sort1.rb
@@ -3,5 +3,6 @@ def counting_sort(arr)
   (arr.min..arr.max).each do |e|
     my_hash[e] = arr.count(e)
   end
-  my_hash
+  my_hash.each { | k,v | my_hash[k] += my_hash[k-1] unless k == 1 }
+  
 end
