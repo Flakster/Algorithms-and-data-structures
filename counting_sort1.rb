35583ca53b4dbd548c6e02595f921dcb27a84f9f
flakster <carlos.santamaria@gmail.com>
2021-01-20 17:50:49 -0500

    Fix a bug. change the condition to sum frequencies

--- counting_sort1.rb
+++ counting_sort1.rb
@@ -3,7 +3,7 @@ def counting_sort(arr)
   (arr.min..arr.max).each do |e|
     my_hash[e] = arr.count(e)
   end
-  my_hash.each { | k,v | my_hash[k] += my_hash[k-1] unless k == 1 }
+  my_hash.each { | k,v | my_hash[k] += my_hash[k-1] unless k == arr.min }
   sorted = Array.new(arr.length)
   arr.each do |e|
     index = my_hash[e]
