91f7d749fe04b6ba3a74817bb1554329c1b69bf8
flakster <carlos.santamaria@gmail.com>
2021-01-21 21:45:15 -0500

    create the file to solve the index match repl.it challenge

---
+++ index_match.rb
@@ -0,0 +1,22 @@
+def index_match(array)
+  # write your code here
+  min = 0
+  max = array.length - 1
+  pivot = max / 2
+  while min < max do
+    p " min: #{min} max: #{max} pivot: #{pivot}"
+    if array[pivot] > pivot
+      max = pivot
+    elsif array[pivot] < pivot
+      min = pivot
+    else
+      return pivot 
+    end
+    pivot = (min + max) / 2
+    gets
+  end
+  return -1
+end
+
+puts index_match([0, 2, 3, 7, 9, 11])
+# => 0
