2464c9aebbe9bcbafa3ba6349a355654177196a3
flakster <carlos.santamaria@gmail.com>
2021-04-23 22:06:17 -0500

    Remove unnecessary comments

--- cardsCastle.rb
+++ cardsCastle.rb
@@ -1,17 +1,3 @@
-# def cards_needed(n)
-# 	return 'invalid' if n < 0
-	
-# 	cards = 0
-# 	(1..n).each do |level|
-# 		if level =	= 1
-# 			cards += n * 2
-# 		else
-# 			cards += (level-1) * 3
-# 		end
-# 	end
-# 	return cards
-# end
-
 def cards_needed(n)
 	n.negative? ? 'invalid' : 3 * n * (n+1) / 2 - n
 end
